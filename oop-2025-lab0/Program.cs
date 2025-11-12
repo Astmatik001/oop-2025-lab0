f91a2f9710015e2c38a7777c6607bfeaaeb90fd0
Astmatik001 <esoklakov@gmail.com>
2025-11-12 18:05:47 +0300

    Добавьте файлы проекта.

---
+++ oop-2025-lab0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_2025_lab0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
